17f4090bd1584e2d69392c23d0899d227cce1732
nikhilmehlan <145481279+nikhilmehlan@users.noreply.github.com>
2024-06-10 13:42:13 +0530

    Admin Panel

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# Agri-Link
+<br/>
+<p align="center">
+  <a href="https://github.com/varnit-ta/AgriLink">
+    <img src="frontend/src/assets/logo.png" alt="Logo" height="80">
+  </a>
+
+  <h3 align="center">AgriLink</h3>
+
+  <p align="center">
+    A transformative tool empowering farmers to sell directly to consumers while offering blockchain-based insurance for financial security.
+    <br/>
+    <br/>
+  </p>
+</p>
+
+![Contributors](https://img.shields.io/github/contributors/varnit-ta/AgriLink?color=dark-green) ![Forks](https://img.shields.io/github/forks/varnit-ta/AgriLink?style=social)
+
+## About The Project
+
+AgriLink revolutionizes agricultural commerce by enabling farmers to bypass middlemen and directly connect with consumers, fostering fairer trade and higher profits. Our platform builds a vibrant community where farmers showcase their products, bridging the gap between farm and table.
+
+🔗 Leveraging blockchain technology, AgriLink establishes a secure insurance pool, ensuring farmers receive timely payouts with minimal hassle. With blockchain-based insurance, accessing funds is streamlined, eliminating bureaucratic hurdles commonly associated with traditional banking systems.
+
+## Built With
+
+- Built with the power of MERN stack for seamless performance and scalability.
+- Smart contracts implemented in Solidity ensure transparency and security in insurance transactions.
+- Stripe integration facilitates smooth and secure payment processing for farmers and buyers alike.
+
+## Getting Started
+
+1. **Clone Repository:** 
+   Clone the AgroInsight repository from GitHub and navigate to the project directory.
+
+2. **Install Dependencies:** 
+   Navigate to the "Frontend" and "Backend" directories and run `npm install` to install the dependencies.
+
+3. **Set Up Environment:** 
+   Configure environment variables in the `.env` file following the provided template.
+   - **In the backend:** Add a `MONGO_URI`, `PORT` (assume 5000), and a `SECRET` (for JWT verification).
+   - **In the frontend:** Add the `BACKEND_URI` (address and port of the backend server) and `MODEL_URI` (address and port of the python model server).
+
+4. **Start Servers:**
+   - **Starting backend:** Launch the backend server with `node server` in the "Backend" directory. If you have nodemon installed, run `nodemon server` for automatic restarts on file changes.
+   - **Starting frontend:** Launch the frontend server with `npm run dev` in the "Frontend" directory.
+   - **Starting the Python server:** Start the Python server containing the neural model by running `uvicorn main:app --reload --port 8000`.
+
+5. **Explore Application:** 
+   Access AgroInsight to begin exploring and customizing the application for your crop monitoring needs.
+
+
+> [!NOTE]
+> **Note:** Before running the project, ensure that you have all necessary dependencies installed and configured according to the instructions provided in the "Getting Started" section.
+
+> [!TIP]
+> **Tip:** For the best experience, consider running the project on a modern web browser such as Google Chrome or Mozilla Firefox.
+
+> [!IMPORTANT]
+> **Important:** Make sure to set up the environment variables correctly in the `.env` file before starting the backend and frontend servers.
+
+> [!WARNING]
+> **Warning:** Running the project without proper configuration or environment setup may result in unexpected errors or incomplete functionality.
+
+> [!CAUTION]
+> **Caution:** Be cautious when modifying sensitive configuration settings or environment variables, as incorrect changes could lead to security vulnerabilities or data loss.
+
+
+## Acknowledgements
+
+* [Varnit Singh](https://github.com/varnit-ta/)
+* [Nikhil Mehlan](https://github.com/nikhilmehlan)
+* [Ansh Singhal](https://github.com/ansh1331)
+* [Lokesh Maheswari]()
+* [Gautam Arora]()
---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import './Add.css'
+import { assets,url } from '../../assets/assets';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const Add = () => {
+
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+    });
+
+    const [image, setImage] = useState(false);
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("price", Number(data.price));
+        formData.append("category", data.category);
+        formData.append("image", image);
+        const response = await axios.post(`${url}/api/food/add`, formData);
+        if (response.data.success) {
+            toast.success(response.data.message)
+            setData({
+                name: "",
+                description: "",
+                price: "",
+                category: "Fresh Produce"
+            })
+            setImage(false);
+        }
+        else{
+            toast.error(response.data.message)
+        }
+    }
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    return (
+        <div className='add'>
+            <form className='flex-col' onSubmit={onSubmitHandler}>
+                <div className='add-img-upload flex-col'>
+                    <p>Upload image</p>
+                    <label htmlFor="image">
+                        <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="" />
+                    </label>
+                    <input onChange={(e) => { setImage(e.target.files[0]) }} type="file" id="image" hidden required />
+                </div>
+                <div className='add-product-name flex-col'>
+                    <p>Product name</p>
+                    <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Type here' required />
+                </div>
+                <div className='add-product-description flex-col'>
+                    <p>Product description</p>
+                    <textarea name='description' onChange={onChangeHandler} value={data.description} type="text" rows={6} placeholder='Write content here' required />
+                </div>
+                <div className='add-category-price'>
+                    <div className='add-category flex-col'>
+                        <p>Product category</p>
+                        <select name='category' onChange={onChangeHandler} >
+                            <option value="Fresh Produce">Fresh Produce</option>
+                            <option value="Dairy Products">Dairy Products</option>
+                            <option value="Meat and Poultry">Meat and Poultry</option>
+                            <option value="Grains and Cereals">Grains and Cereals</option>
+                            <option value="Heritage and Heirloom">Heritage and Heirloom</option>
+                            <option value="Value Added Products">Value added products</option>
+                            <option value="Organic and Natural Products">Organic and Natural Products</option>
+                            <option value="Farm Supplies and tools">Farm Supplies and tools</option>
+                        </select>
+                    </div>
+                    <div className='add-price flex-col'>
+                        <p>Product Price</p>
+                        <input type="Number" name='price' onChange={onChangeHandler} value={data.price} placeholder='Rs.25' />
+                    </div>
+                </div>
+                <button type='submit' className='add-btn' >ADD</button>
+            </form>
+        </div>
+    )
+}
+
+export default Add
